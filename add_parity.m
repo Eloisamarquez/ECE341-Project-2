04414c11387db834a2d6723d2e2f386c6870a359
Eloisa <60202894+Eloisamarquez@users.noreply.github.com>
2020-07-26 00:13:23 -0500

    fit binary addition

--- add_parity.m
+++ add_parity.m
@@ -4,9 +4,9 @@ for i = 1: 3: length(input)
     for j = 0: 2
         parity_input((2 * i -1) + j) = input(i + j);
     end
-    parity_input((2 * i -1) + 3) = input(i) + input(i + 1);
-    parity_input((2 * i -1) + 4) = input(i) + input(i + 2);
-    parity_input((2 * i -1) + 5) = input(i + 1) + input(i + 2);
+    parity_input((2 * i -1) + 3) = bitget(input(i) + input(i + 1), 1);
+    parity_input((2 * i -1) + 4) = bitget(input(i) + input(i + 2), 1);
+    parity_input((2 * i -1) + 5) = bitget(input(i + 1) + input(i + 2), 1);
 end
 end
 
